f134f7597684ddd7c7fbf1fc112fb339363ce958
anmot <a.motsyk@interactivity.com.ua>
2015-09-14 15:36:10 +0300

    Angular working

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,31 @@
-var AppComponent = ng
-    .Component({
-      selector: 'my-app'
-    })
-    .View({
-      template: '<h1 id="output">My First Angular 2 App</h1>'
-    })
-    .Class({
-      constructor: function () { }
-    });
+// var AppComponent = ng
+//     .Component({
+//       selector: 'my-app'
+//     })
+//     .View({
+//       template: '<h1 id="output">My First Angular 2 App</h1>'
+//     })
+//     .Class({
+//       constructor: function () { }
+//     });
 
-console.log('normal');
+// console.log('normal');
+
+// ES5
+
+function DisplayComponent() {
+  this.myName = "Alice";
+}
+DisplayComponent.annotations = [
+  new ng.ComponentAnnotation({
+    selector: "my-app"
+  }),
+  new ng.ViewAnnotation({
+    template:
+       '<p>My name: {{ myName }}</p>'
+  })
+];
+
+document.addEventListener('DOMContentLoaded', function() {
+  ng.bootstrap(DisplayComponent);
+});
