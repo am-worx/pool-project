b4329d0dfde9d88f89af26c78cfc0c68cdabaf9a
anmot <a.motsyk@interactivity.com.ua>
2015-09-14 18:45:32 +0300

    serving one file. Need Express

--- src/index.js
+++ src/index.js
@@ -7,29 +7,19 @@ var server = http.createServer(function (req, res) {
     return res.end('Need GET request.\n');
 
   var parsedReq = url.parse(req.url, true);
-  var reqDate = new Date(/*parsedReq.query.iso*/);
-  var result = 0;
 
   if (parsedReq.pathname === '/') {
     fs.readFile('index.html', 'utf8', function (err, data) {
-		  if (err) {throw err; console.log('read error');}
-		  result = data;
+		  if (err) {
+        throw err; 
+        console.log('read error');
+      } else {
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write(data, 'utf8');
+        res.end();
+      }
 
-		  res.writeHead(200);
-      res.write(data + '123', "utf8");
-      res.end();
 		});
-  }
-  else if (parsedReq.pathname === '/api/parsetime') {
-    result = {
-      hour : reqDate.getHours(),
-      minute : reqDate.getMinutes(),
-      second : reqDate.getSeconds() 
-    }
-  } else if (parsedReq.pathname === '/api/unixtime') {
-    result = {
-      unixtime : reqDate.getTime()
-    }
   } else {
     res.writeHead(404)
     res.end();
@@ -42,4 +32,4 @@ var server = http.createServer(function (req, res) {
   //res.end(result);
 });
 
-server.listen(8081);
+server.listen(8080);
